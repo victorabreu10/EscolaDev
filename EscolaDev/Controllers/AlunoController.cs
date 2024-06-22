317597e785dd71df6b5c0428e1a30e4e74f06ff2
Gelson Junior <gelsonpicklis@hotmail.com>
2024-06-22 12:32:28 -0300

    Feature/AtualizarAluno

--- EscolaDev/Controllers/AlunoController.cs
+++ EscolaDev/Controllers/AlunoController.cs
@@ -6,7 +6,7 @@ using System.Data.SqlClient;
 
 namespace EscolaDev.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("Api/[controller]")]
     [ApiController]
     public class AlunoController : ControllerBase 
     {
@@ -20,6 +20,7 @@ namespace EscolaDev.Controllers
         {
             _connectionString = configuration.GetConnectionString("DataBase");  
         }
+
         /// <summary>
         /// Busca todos os alunos existentes
         /// </summary>
@@ -34,6 +35,7 @@ namespace EscolaDev.Controllers
                 return Ok(aluno);
             }
         }
+
         /// <summary>
         /// Realiza a busca do aluno pelo identificador id 
         /// </summary>
@@ -52,6 +54,11 @@ namespace EscolaDev.Controllers
             }
         }
 
+        /// <summary>
+        /// Cria um aluno no Banco de dados
+        /// </summary>
+        /// <param name="Criar"></param>
+        /// <returns></returns>
         [HttpPost("Criar")]
         public async Task<IActionResult> CriarAluno(Aluno criar)
         { 
@@ -64,6 +71,26 @@ namespace EscolaDev.Controllers
                 const string sql = "INSERT INTO dbo.Aluno VALUES (@NomeAluno, @DataAniversario, @EstaAtivo, @NomeEscola)";
 
                 var response = await sqlconnection.ExecuteAsync(sql, parameters);
+                return Ok(parameters);
+            }
+        }
+
+        /// <summary>
+        /// Atualiza um aluno expecifico no Banco de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="aluno"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<ActionResult> AtualizarAluno(int id, Aluno aluno)
+        {
+            var parameters = new {id, aluno.NomeAluno, aluno.DataAniversario, aluno.NomeEscola};
+
+            using (var sqlconnection = new SqlConnection(_connectionString))
+            {
+                const string sql = "UPDATE Aluno SET NomeAluno = @NomeAluno, DataAniversario = @DataAniversario, NomeEscola = @NomeEscola  WHERE Id = @id";
+
+                await sqlconnection.ExecuteAsync(sql, parameters);
                 return Ok(parameters);
             }
         }
